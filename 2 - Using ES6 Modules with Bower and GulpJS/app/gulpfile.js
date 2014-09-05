c2672f82b938a881a12a61bc04092a0091c5133b
Steven Hunt <steven.mtw.hunt@gmail.com>
2014-09-05 16:09:28 -0400

    Added packaging to build system.

--- 2 - Using ES6 Modules with Bower and GulpJS/app/gulpfile.js
+++ 2 - Using ES6 Modules with Bower and GulpJS/app/gulpfile.js
@@ -1,25 +1,26 @@
 
+// include the gulp library.
 var gulp       = require('gulp');
 
-var clean      = require('gulp-clean'),
+// include gulp plugin libraries we'll be using.
+var bowerFiles = require('main-bower-files'),
+	buildApp   = require('./gulp/build-app'),
+    clean      = require('gulp-clean'),
 	concat     = require('gulp-concat'),
 	filter     = require('gulp-filter'),
 	flatten    = require('gulp-flatten'),
 	jshint     = require('gulp-jshint'),
+	rename     = require('gulp-rename'),
 	sourcemaps = require('gulp-sourcemaps'),
-	buildApp   = require('./gulp/build-app');
+	uglify     = require('gulp-uglify'),
+	util       = require('gulp-util');
 
-var bowerFiles = require('main-bower-files');
-
-var env = 'dev'
-	envRuntime = env + "-runtime";
-
-const BOWER_PATH = 'bower_components';
-/*[
-			'bower_components/traceur-runtime/traceur-runtime.js',
-			'bower_components/es6-module-loader/dist/es6-module-loader.js',
-			'bower_components/system.js/dist/system.js'
-		]*/
+const 	PATH_BOWER     = 'bower_components', // path to our Bower files.
+		PATH_APP       = 'app',
+		PATH_BUILD     = 'build',
+		PATH_BUILD_LIB = 'build/lib',
+		PATH_PKG       = 'dist',
+		PATH_PKG_LIB   = 'dist/lib';
 
 // Cleans the build directory before running the build.
 gulp.task('clean', function () {
@@ -29,48 +30,94 @@ gulp.task('clean', function () {
 
 // Applies linting to the app source code.
 gulp.task('lint', function () {
-    return gulp.src('app/*.js')
-		.pipe(jshint({ esnext: true }))
+    return gulp.src(PATH_APP + '/*.js')
+		.pipe(jshint({esnext: true}))
 	    .pipe(jshint.reporter('jshint-stylish'))
 		.pipe(jshint.reporter('fail'));
 });
 
-gulp.task('copy-files', ['clean'], function () {
+// Copies html file to the build.
+gulp.task('build-files', ['clean'], function () {
 	return gulp.src('index.html')
-		.pipe(gulp.dest('build'));
+		.pipe(gulp.dest(PATH_BUILD));
 });
 
 // Select and concatenate our runtime dependencies.
 gulp.task('build-runtime', ['clean'], function () {
-	return gulp.src(bowerFiles({ env: envRuntime }), { base: BOWER_PATH })
+	return gulp.src(bowerFiles({ env: 'dev-runtime' }), { base: PATH_BOWER })
+		.pipe(sourcemaps.init())
 		.pipe(concat('runtime.js'))
-		.pipe(gulp.dest('build'));
+        .pipe(sourcemaps.write())
+		.pipe(gulp.dest(PATH_BUILD));
 });
 
-// Copy our dependencies into the /lib folder.
+// Copy our library dependencies into the /lib folder.
 gulp.task('build-lib', ['clean'], function () {
-	return gulp.src(bowerFiles({ env: env }), { base: BOWER_PATH })
+	return gulp.src(bowerFiles({ env: 'dev' }), { base: PATH_BOWER })
 		.pipe(flatten())
-		.pipe(gulp.dest('build/lib'));
+		.pipe(gulp.dest(PATH_BUILD_LIB));
 });
 
-// Transpiles our app's ES6 source code into a single JS file.
+// Transpile our app's ES6 source code into a single JS file.
 gulp.task('build-app', ['clean'], function () {
-    return gulp.src('app/*.js', {base: 'app'})
+    return gulp.src(PATH_APP + '/*.js', {base: PATH_APP})
 		.pipe(sourcemaps.init())
 		.pipe(buildApp())
 		.pipe(concat('app.js'))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest(PATH_BUILD));
 });
 
-// The master build task.
+// Runs the entire build.
 gulp.task('build', [
 	'lint', 
-	'copy-files', 
+	'build-files', 
 	'build-runtime', 
 	'build-lib', 
 	'build-app']);
 
-// The default task AKA build.
+// Cleans the packaging directory before running the build.
+gulp.task('package-clean', ['build'], function () {
+    return gulp.src(PATH_PKG, {read: false})
+        .pipe(clean());
+});
+
+// Copy js files out of the build.
+gulp.task('package-files', ['package-clean'], function () {
+	return gulp.src('index.html')
+		.pipe(gulp.dest(PATH_PKG));
+});
+
+// Build the runtime file using the production (minified) files.
+gulp.task('package-runtime', ['package-clean'], function () {
+	return gulp.src(bowerFiles({ env: 'prod-runtime' }), { base: PATH_BOWER })
+		.pipe(concat('runtime.js'))
+		.pipe(gulp.dest(PATH_PKG));
+});
+
+// Copy the dependencies using the production (minified) files.
+gulp.task('package-lib', ['package-clean'], function () {
+	return gulp.src(bowerFiles({ env: 'prod' }), { base: PATH_BOWER })
+		.pipe(flatten())
+		.pipe(rename(function (path) {
+        	path.basename = path.basename.replace('.min', '');
+		}))
+		.pipe(gulp.dest(PATH_PKG_LIB));
+});
+
+// Minify the app code.
+gulp.task('package-app', ['package-clean'], function () {
+	return gulp.src(PATH_BUILD + '/app.js')
+		.pipe(uglify())
+		.pipe(gulp.dest(PATH_PKG));
+});
+
+// Runs the entire build and package.
+gulp.task('package', [
+	'package-files',
+	'package-runtime',
+	'package-lib',
+	'package-app']);
+
+// The default task: build.
 gulp.task('default', ['build']);
