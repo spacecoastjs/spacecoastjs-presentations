b4db6630014cf5d4965c37b8a85c179ba48329c6
Steven Hunt <steven.mtw.hunt@gmail.com>
2014-11-07 01:03:06 -0500

    First step of presentation, basic app.

--- 4 - Build Systems/calculator.js
+++ 4 - Build Systems/calculator.js
@@ -1,27 +1,44 @@
 
-var calculator = {};
+calculator = {};
 
+/**
+ * @public
+ * Adds the given numbers.
+ */
 calculator.add = function (num1, num2) {
 	return -num1 + num2;
-};
+}
 
+/**
+ * @public
+ * Subtracts the given numbers.
+ */
 calculator.subtract = function (num1, num2) {
 	return num1 - num2;
-};
+}
 
+/**
+ * @public
+ * Multiplies the given numbers.
+ */
 calculator.multiply = function (num1, num2) {
 	return num1 * num2;
-};
+}
 
+/**
+ * @public
+ * Divides the given numbers.
+ */
 calculator.divide = function (num1, num2) {
 	return num1 / num2;
-};
+}
 
 // if we're in a browser, add to the global Window object.
 if (window !== undefined) {
 	window.calculator = calculator;
 }
-// if we're in CommonJS, export the calculator functions.
+
+// if we're in CommonJS, export the calculator object.
 else if (module !== undefined) {
 	module.exports = calculator;
 }
